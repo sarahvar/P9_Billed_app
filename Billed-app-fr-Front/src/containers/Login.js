0ed6d3b412ddd8dabc0efc21a08b0ec7314ff8bc
sarahvar <sarahvarin95@gmail.com>
2023-12-27 22:20:53 +0100

    submitAdmin

--- Billed-app-fr-Front/src/containers/Login.js
+++ Billed-app-fr-Front/src/containers/Login.js
@@ -41,7 +41,7 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`form[data-testid="admin-email-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
